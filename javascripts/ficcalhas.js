078c831308e6ac3464fc905f794a4c2aca9cf517
Jefte Amorim da Costa <jefteamorim@gmail.com>
2015-11-01 21:50:51 -0200

    atualização do js ficcalhas

--- javascripts/ficcalhas.js
+++ javascripts/ficcalhas.js
@@ -6,4 +6,23 @@ $(document).on('click','#header-busca button',function(){
       });
 $(document).on('click','button.remove',function(){
   $( "#shide" ).animate({top:'-80px'},"slow");
+
       });
+$(document).ready(function(){
+    $("#flip").click(function(){
+    	$(".pull-left").slideUp("slow");
+        $(".pull-right").slideUp("slow");
+        $("#panel").slideDown("slow");
+        
+    });
+   
+});
+$(document).ready(function(){
+    $("#flip2").click(function(){
+    	$(".pull-left").slideDown("slow");
+        $(".pull-right").slideDown("slow");
+        $("#panel").slideUp("slow");
+        
+    });
+   
+});
